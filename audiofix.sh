d7de576e5078e3cf28908be1bf7fde9dc8730567
Pablo Rozas <46354669+skoglar@users.noreply.github.com>
2019-01-23 23:00:35 -0300

    Update audiofix.sh

--- audiofix.sh
+++ audiofix.sh
@@ -1,3 +1,3 @@
 printf "pcm.pulse {    type pulse\n}\nctl.pulse {    type pulse\n}\n" > /storage/.config/asound.conf
 mount -o remount rw /flash/
-printf "\n#LE: DEACTIVATE ALSA:\ndtparam=audio=on\n" >> /flash/config.txt
+printf "\n#LE: ACTIVATE ALSA:\ndtparam=audio=on\n" >> /flash/config.txt
