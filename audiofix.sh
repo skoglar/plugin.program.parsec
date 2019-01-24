cb275a6d371fbb91d620c0b674d603ac2be6af7d
Pablo Rozas <46354669+skoglar@users.noreply.github.com>
2019-01-23 23:02:18 -0300

    Update audiofix.sh

--- audiofix.sh
+++ audiofix.sh
@@ -1,3 +1,6 @@
+#Create alsa config file to connect to pa
 printf "pcm.pulse {    type pulse\n}\nctl.pulse {    type pulse\n}\n" > /storage/.config/asound.conf
+#remount /flash partition in read-write mode
 mount -o remount rw /flash/
+#append dtparam=audio=on to config.txt
 printf "\n#LE: ACTIVATE ALSA:\ndtparam=audio=on\n" >> /flash/config.txt
